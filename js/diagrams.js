b8b6a2df00e58b712c7f72c05ec91c760914ec90
Ziyang Zhou <bbq379@hotmail.com>
2020-12-17 04:30:03 +0100

    fix conflict

--- js/diagrams.js
+++ js/diagrams.js
@@ -432,12 +432,19 @@ function refresh() {
 }
 
 d3.select("#play-button").on("click", function() {
-    var button = d3.select(this);
-    if (button.text() == "Pause") {
-        moving = false;
+    let button = d3.select(this);
+    let moving = false, timer = 0;
+    if (button.text() === "Pause") {
         clearInterval(timer);
         // timer = 0;
         button.text("Play");
+    } else {
+        moving = true;
+        timer = setInterval(step, 200);
+        button.text("Pause");
+    }
+    console.log("Slider moving: " + moving);
+})
 
 refresh()
 d3.select(window).on('resize', refresh);
