7c23e473342da33825639830aed0aa47848eb6cb
Ziyang Zhou <bbq379@hotmail.com>
2021-02-01 21:51:40 +0100

    minor fix

--- js/graphs/mark_map.js
+++ js/graphs/mark_map.js
@@ -1,20 +1,7 @@
 function mark_map(param) {
     for (let state of states) {
-        // let map_label = d3.select("#indicator_" + state);
         let map_label = d3.select("#jqvmap1_" + state + "_pin");
         map_label.html(param.data[param.date][state])
-        // let incidence = param.data[param.date][state]
-        // if (incidence <= 0) {
-        //     map_label.attr("class", "center_box map_indicator map_indicator0");
-        // } else if (incidence > 0 && incidence <= 40) {
-        //     map_label.attr("class", "center_box map_indicator map_indicator1");
-        // } else if (incidence > 40 && incidence <= 80) {
-        //     map_label.attr("class", "center_box map_indicator map_indicator2");
-        // } else if (incidence > 80 && incidence <= 120) {
-        //     map_label.attr("class", "center_box map_indicator map_indicator3");
-        // } else {
-        //     map_label.attr("class", "center_box map_indicator map_indicator4");
-        // }
 
         let map_title = d3.select("#map_title")
         let start_date = d3.timeParse(rki_dateFormat)("2020-01-01")
