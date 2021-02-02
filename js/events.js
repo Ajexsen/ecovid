e8bb37c206d95d3c9fdc151e37e15778c40d87a1
Ziyang Zhou <bbq379@hotmail.com>
2021-02-02 04:23:47 +0100

    map deselect

--- js/events.js
+++ js/events.js
@@ -14,3 +14,10 @@ d3.select("#play-button").on("click", function () {
     }
     // console.log("Slider moving: " + moving);
 })
+
+d3.select("#map_deselect").on("click", function () {
+    let states = $("#map .map_svg .jqvmap-region");
+    states.attr('class', 'jqvmap-region');
+    data_source = de_data_source;
+    render_graph();
+})
